706cd303a07844b490daa964e14a72e1b0a84d6b
Leonardo Nascimento <98924684+Leonardograut@users.noreply.github.com>
2024-07-01 16:56:43 -0300

    Add files via upload

---
+++ src/desafios/repeticoes2/exer03.java
@@ -0,0 +1,18 @@
+package repeticoes2;
+
+public class exer03 {
+
+	public static void main(String[] args) {
+	
+		int limite = 0;
+		
+		
+		for (int i = 0;  limite <=50; i++) {
+			if (limite %2 !=0) {
+				System.out.println(limite + " ");
+			}
+			limite++;
+		}
+	}
+
+}
---
+++ src/desafios/repeticoes2/exer05.java
@@ -0,0 +1,17 @@
+package repeticoes2;
+
+public class exer05 {
+
+	public static void main(String[] args) {
+		
+		//imprima todos os multiplos de  3 , entre 1 e 100
+		for (int i = 1; i <= 100; i++) {
+			if (i %3 ==0) {
+				System.out.println(i + " ");
+			}
+			
+		}
+
+	}
+
+}
