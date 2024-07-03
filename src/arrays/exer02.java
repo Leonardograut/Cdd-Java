4152d6a04ff5250cd672b594b408d2533cdb2652
leonardograut <omgleo@live.com>
2024-07-03 00:25:18 -0300

    exercicio 03

--- src/arrays/exer02.java
+++ src/arrays/exer02.java
@@ -6,35 +6,33 @@ import java.util.Scanner;
 receba uma nota de 5 alunos, 
 guarde todas  num array notas.
  Depois calcule a média da turma. 
-  */
+ */
 
 public class exer02 {
 
 	public static void main(String[] args) {
-		 int[] notas = new int[5];
-		 double soma =0;
-		 
-		 double media;
-		 
-		 Scanner sc = new Scanner(System.in);
-		 
-		 
-		 for (int i = 0; i < notas.length; i++) {
+		int[] notas = new int[5];
+		double soma =0;
+
+		double media;
+
+		Scanner sc = new Scanner(System.in);
+
+
+		for (int i = 0; i < notas.length; i++) {
 			System.out.println("Digite a nota  do aluno");
 			notas[i] = sc.nextInt();
 			soma = soma + notas[i];
-			
-		}
-		 
-		 media = soma /notas.length;
-		 
-		 System.out.println("A media entre os alunos e "+ media);
-		 
-		 
-		 
-		 sc.close();
+
+		}		 
+		media = soma /notas.length;
+
+		System.out.println("A media entre os alunos e "+ media);
+
+
+
+		sc.close();
 	}
 
-	
-	
+
 }
---
+++ src/arrays/exer03.java
@@ -0,0 +1,22 @@
+package arrays;
+
+
+/*
+ Escreva um código 
+Java que exiba os valores de um 
+array com o nome a do tipo double 
+numa mesma linha
+*/
+
+public class exer03 {
+
+	public static void main(String[] args) {
+		double[] a = {1.0,2.2,3.3,4.5,5.6,7.6};
+		
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i]+" ");
+		}
+
+	}
+
+}
