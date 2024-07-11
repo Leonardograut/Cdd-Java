a14b9a69be1842e2f70f8aaf25d5c82ea5c06f9f
Leonardo Nascimento <98924684+Leonardograut@users.noreply.github.com>
2024-07-11 16:40:27 -0300

    Add files via upload

---
+++ src/poo/ClasseTeste.java
@@ -0,0 +1,24 @@
+package javapoo;
+
+public class ClasseTeste {
+
+	public static void main(String[] args) {
+		
+		ClassePessoa sc = new ClassePessoa();
+		
+		sc.nome = "Leonardo";
+		
+		//System.out.print(sc.nome + " ");
+		
+		sc.andar();
+		sc.comer();
+		sc.dormir();
+		sc.pararDeandar();
+		
+	
+		
+		
+		
+	}
+
+}
