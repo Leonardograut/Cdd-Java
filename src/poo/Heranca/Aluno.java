53bd799e2113722b275da87367af629b142208ee
Leonardo Nascimento <98924684+Leonardograut@users.noreply.github.com>
2024-07-18 16:49:52 -0300

    exemplo herança  java

---
+++ src/poo/Heranca/Aluno.java
@@ -0,0 +1,14 @@
+package Heranca;
+
+public class Aluno extends Pessoa {
+
+	public String matricula;
+	
+	public Aluno(String nome, int idade ) {
+		
+		super(nome, idade );
+		
+	}
+
+	
+}
---
+++ src/poo/Heranca/Funcionario.java
@@ -0,0 +1,14 @@
+package Heranca;
+
+public class Funcionario extends Pessoa {
+
+	public double salario;
+	public String cargo;
+	
+	
+	public Funcionario(String nome, int idade) {
+		super(nome, idade);
+		// TODO Auto-generated constructor stub
+	}
+
+}
