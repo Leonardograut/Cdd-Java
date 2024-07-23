32d20a31ab014749eec2e3d4ce9b69f15188523a
Leonardo Nascimento <98924684+Leonardograut@users.noreply.github.com>
2024-07-23 15:44:30 -0300

    exercicio polimorfismo

---
+++ src/polimorfismo/Formas.java
@@ -0,0 +1,17 @@
+package polimorfismo;
+
+public  abstract class Formas {
+	
+	public void  calcArea() {
+		System.out.println();
+    }
+		
+	
+	
+	public void calcPerimetro() {
+		System.out.println();
+	}
+	
+	
+	
+}
---
+++ src/polimorfismo/Geometrica.java
@@ -0,0 +1,21 @@
+package polimorfismo;
+
+public class Geometrica {
+
+	public static void main(String[] args) {
+		
+		Triangulo t1 = new Triangulo();
+		
+	    t1.calceArea(15, 6);
+		t1.calcPerimetro(5);
+		t1.calcPerimetro(4, 6, 8);
+		
+		System.out.printf("O Calculo da Area ");
+		
+		System.out.println("O calculo do Perimetro" );
+		
+		System.out.println("O Calculo de Perimetro");
+
+	}
+
+}
